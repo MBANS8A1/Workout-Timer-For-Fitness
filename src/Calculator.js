e80bf86f3567e5861a7a01447198a1c8b84785d5
MBANS8A1 <seanmbng@aol.com>
2025-12-23 21:55:36 +0000

    I added another effect (in Calculator.js) to change the title of the application in the browser tab when the number of exercises for a specific workout changes.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { useState, memo, useEffect, useCallback } from "react";
+import { useState, memo, useEffect } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -11,15 +11,6 @@ function Calculator({ workouts, allowSound }) {
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
-
-  // const playSound = useCallback(
-  //   function () {
-  //     if (!allowSound) return;
-  //     const sound = new Audio(clickSound);
-  //     sound.play();
-  //   },
-  //   [allowSound]
-  // );
 
   useEffect(
     function () {
@@ -38,6 +29,15 @@ function Calculator({ workouts, allowSound }) {
       playSound();
     },
     [duration, allowSound]
+  );
+
+  //For demonstrating closures (just an example)
+  useEffect(
+    function () {
+      //number of exercises in title
+      document.title = `Your ${number}-exercise workout`;
+    },
+    [number]
   );
 
   function handleIncrement() {
